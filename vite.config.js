bab4db2c2334da93263b7eb6706db10b4b59d891
Savaliya Manish <savaliyamanish1141@gmail.com>
2025-08-23 14:53:31 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default ({mode}) => {
 
   return defineConfig({
     plugins: [vue()],
-    base:"/other-projects",
+    base:  '/',
     resolve: {
       alias: [
         // thanks to https://stackoverflow.com/a/67676242/22296059 :)
