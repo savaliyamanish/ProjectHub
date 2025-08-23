fa99c53126a208389e3580a9bf59e2b38f2e948c
Savaliya Manish <savaliyamanish1141@gmail.com>
2025-08-23 14:04:38 +0530

    Initial commit
    Initial commit

---
+++ src/views/GameProjects.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Stuff</h1>
+
+    <div style="margin-bottom: 30px;">
+      The following are some stuff I've made or heavily contributed to.
+    </div>
+
+    <ProjectsList v-bind:projects="projects" />
+
+    <div style="margin-top: 20px;">
+      There is more to see on <a target="_blank" href="https://someexternalwebsite.com">some external website</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ProjectsList from "@/components/ProjectsList.vue";
+
+export default defineComponent({
+  name: "GameProjects",
+  components: {
+    ProjectsList,
+  },
+  data: function () {
+    return {
+      projects: [] as Array<{ id: string; name: string; htmlDescription: string; iconUrl: string; isWide: boolean; isHigh: boolean; accentColor: string }>,
+    };
+  },
+  created: async function () {
+    const response = await fetch("/projectList.json");
+    const projectList = await response.json();
+    const baseUrl = projectList.baseUrl;
+    const projectPromises = projectList.gameProjects.map(async (id: string) => {
+      const projectResponse = await fetch(`${baseUrl}${id}/data.json`);
+      return projectResponse.json();
+    });
+    this.projects = await Promise.all(projectPromises);
+  },
+});
+</script>
---
+++ src/views/OtherProjects.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Other stuff</h1>
+
+    <div style="margin-bottom: 30px;">
+      And here are some other stuff I've made or heavily contributed to, which are totally different than the stuff from earlier.
+    </div>
+    <ProjectsList v-bind:projects="projects" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ProjectsList from "@/components/ProjectsList.vue";
+
+export default defineComponent({
+  name: "OtherProjects",
+  components: {
+    ProjectsList,
+  },
+  data: function () {
+    return {
+      projects: [] as Array<{ id: string; name: string; htmlDescription: string; iconUrl: string; isWide: boolean; isHigh: boolean; accentColor: string }>,
+    };
+  },
+  created: async function () {
+    const response = await fetch("/projectList.json");
+    const projectList = await response.json();
+    const baseUrl = projectList.baseUrl;
+    const projectPromises = projectList.otherProjects.map(async (id: string) => {
+      const projectResponse = await fetch(`${baseUrl}${id}/data.json`);
+      return projectResponse.json();
+    });
+    this.projects = await Promise.all(projectPromises);
+  },
+});
+</script>
