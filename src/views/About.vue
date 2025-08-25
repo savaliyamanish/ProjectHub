b273daad8f1f629057860d0ac1cb389e2d37571c
Savaliya Manish <savaliyamanish1141@gmail.com>
2025-08-25 16:14:29 +0530

    Fix

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,7 +14,7 @@
     </div>
 
     <div class="photo">
-      <img :src="`/img/avatar.png`" alt="Avatar of John" /> 
+      <img :src="`${import.meta.env.BASE_URL}img/avatar.png`" alt="Avatar of John" /> 
     </div>
 
     <div style="clear:both"></div>
