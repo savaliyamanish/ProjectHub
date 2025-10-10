c16a33fd7dd9dfae9edb69f734160dd81b82fa9c
Savaliya Manish <savaliyamanish1141@gmail.com>
2025-10-10 13:53:56 +0530

    Fix URL issue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter as _createRouter, createWebHistory } from 'vue-router'
+import { createRouter as _createRouter, createWebHashHistory } from 'vue-router'
 
 // route-level code splitting
 const AboutView = () => import('../views/About.vue');
@@ -9,8 +9,8 @@ const ContactView = () => import('../views/Contact.vue')
 const E404View = () => import('../views/404.vue')
 
 export function createRouter(){
-  return _createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+  return _createRouter({    
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
       {
         path: '/',
